8083a2caf5d15f025c66748e8f80ab0a589e0db5
viefimov <trooper.wers@yandex.ru>
2025-03-23 19:23:44 +0300

    hope last fixes

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
 import BadRequestError from '../errors/bad-request-error'
-import faker from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 export const uploadFile = async (
     req: Request,
     res: Response,
@@ -12,23 +12,22 @@ export const uploadFile = async (
     }
     try {
         // Генерация нового уникального имени файла без оригинального имени
-        const generatedFileName = `${faker.string.uuid()}.jpg` 
+        const generatedFileName = `${faker.string.uuid()}.jpg`
 
         const filePath = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${generatedFileName}`
             : `/${generatedFileName}`
 
-        console.log('File uploaded successfully:', filePath);
+        console.log('File uploaded successfully:', filePath)
 
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName: filePath,
             originalName: req.file?.originalname,
         })
     } catch (error) {
-        console.error('Ошибка при загрузке файла:', error);
+        console.error('Ошибка при загрузке файла:', error)
         return next(error)
     }
 }
-
 
 export default {}
